9543deaac0b838acffc347319ad76f79b623c9cc
Timon Kayser <timonkayser@icloud.com>
2021-07-13 11:18:35 +0200

    Punktesystem Logik aufs Backend gebracht

--- src/main/java/de/tkayser/quiz/Answer.java
+++ src/main/java/de/tkayser/quiz/Answer.java
@@ -2,8 +2,12 @@ package de.tkayser.quiz;
 
 public class Answer {
     public Boolean correct;
+    public Integer points;
+    public Integer questionPoints;
 
-    public Answer(boolean correct) {
+    public Answer(boolean correct, int points, int questionPoints) {
         this.correct = correct;
+        this.points = points;
+        this.questionPoints = questionPoints;
     }
 }
--- src/main/java/de/tkayser/quiz/SimpleController.java
+++ src/main/java/de/tkayser/quiz/SimpleController.java
@@ -3,38 +3,70 @@ package de.tkayser.quiz;
 import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.io.*;
 import java.util.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080", methods = {RequestMethod.GET, RequestMethod.POST})
+@CrossOrigin(origins = "http://localhost:8080", methods = {RequestMethod.GET, RequestMethod.POST}, allowCredentials = "true")
 public class SimpleController {
 
     private Random RANDOMIZER = new Random();
 
     @GetMapping(path = "/", produces =  "application/json")
-    public Welcome welcome() {
+    public Welcome welcome(HttpSession session) {
+
+        session.setAttribute("foo", "bar");
         Welcome welcome = new Welcome();
         welcome.message = "Willkommen beim Quiz!";
         return welcome;
     }
 
     @PostMapping(path = "/questions", produces =  "application/json", consumes = "application/json")
-    public Answer question(@RequestBody Data data ) throws IOException {
+    public Answer question(@RequestBody Data data, HttpSession session) throws IOException {
         try (InputStreamReader reader = new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("database.json"))) {
             QuizData quiz = loadQuizData(reader);
             for (int i = 0; i < quiz.daten.length; i++) {
                 QuizQuestion quizQuestion = quiz.daten[i];
                 if (quizQuestion.question.equals(data.frage)) {
                     if (quizQuestion.answers[quizQuestion.right].equals(data.antwort)) {
-                        return new Answer(true);
+                        Integer points = getPointsFromSession(session);
+                        session.removeAttribute("points");
+                        session.removeAttribute("checked");
+                        return new Answer(true, points, 3);
                     } else {
-                        return new Answer(false);
+                        List<String> checked = getCheckedFromSession(session);
+                        Integer points = getPointsFromSession(session);
+                        if (points > 0 && !checked.contains(data.antwort)) {
+                            points--;
+                            checked.add(data.antwort);
+                        }
+                        session.setAttribute("points", points);
+                        session.setAttribute("checked", checked);
+                        return new Answer(false, 0, points);
                     }
                 }
             }
-            return new Answer(false);
+            return new Answer(false, 0, 0);
         }
+    }
+
+    private Integer getPointsFromSession(HttpSession session) {
+        Integer points = (Integer) session.getAttribute("points");
+        System.out.println(session + ": " + points);
+        if (points == null) {
+            points = Integer.valueOf(3);
+        }
+        return points;
+    }
+
+    private List<String> getCheckedFromSession(HttpSession session) {
+        List<String> checked = (List<String>) session.getAttribute("checked");
+        System.out.println(session + ": " + checked);
+        if (checked == null) {
+            checked = new ArrayList<String>();
+        }
+        return checked;
     }
 
     @GetMapping(path = "/quiz", produces =  "application/json")
